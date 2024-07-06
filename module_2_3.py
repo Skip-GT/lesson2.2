17208baa4a49ab6f76dde3bd5720c35702c2a141
Skip-GT <yaroslav.vasilev.2015@gmail.com>
2024-07-06 16:15:36 +0300

    Add files via upload

--- module_2_3.py
+++ module_2_3.py
@@ -1,8 +1,14 @@
 my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 index = 0
 while index < len(my_list):
-    if my_list[index] > 0:
-        print(my_list[index])
-    elif my_list[index] < 0:
-        break
+    a = my_list[index]
     index += 1
+    if a == 0:
+        continue
+    elif a < 0:
+        break
+    print(a)
+    #elif index == len(my_list):
+        #print(a)
+    #else:
+        #print(a)
